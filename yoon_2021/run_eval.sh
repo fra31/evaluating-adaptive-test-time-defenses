8dee516c7e2fd3d354fd59010d0f17cc37782da6
fra31 <francesco91.croce@gmail.com>
2022-03-20 18:56:35 +0000

    update run_eval.sh for adp defense

--- yoon_2021/run_eval.sh
+++ yoon_2021/run_eval.sh
@@ -7,18 +7,16 @@
 dataset='cifar10'
 
 data_path=" --data_dir /scratch/datasets/CIFAR10"
-imgs_path=' --data_path <path>' # path to images to test -- if None clean images are used
-
-#n_ex=1000
-#bs=200
+model_dir=' --model_dir ../../robustbench-addmodels/robustbench/models' # path to robustbench models
+ebm_dir=' --ebm_dir ../../test_time_defenses_eval/adp' # path ebm model
+imgs_path=' --data_path ./results/Standard/apgd_1_1000_niter_10_loss_cw_nrestarts_1_init_None_sigma_0.25_maxiter_10_eotiter_200_0-45_best.pth' # path to images to test - if None clean images are used
 
 
 # run apgd+eot on the full defense (using the standard model from robustbench as base classifier)
-CUDA_VISIBLE_DEVICES=0 python3 eval_tta.py --dataset cifar10 $data_path --n_ex 1000 --batch_size 45 --model_dir ../../robustbench-addmodels/robustbench/models --config ./configs/cifar10_bpda_eot_sigma025_eot15.yml --eot_test 200 --attack apgd --loss cw --n_iter 10 --sigma_def .25 --max_iters_def 10 --model Standard --indices 0-45
+CUDA_VISIBLE_DEVICES=0 python3 eval_tta.py --dataset cifar10 $data_path --n_ex 1000 --batch_size 45 $model_dir --config ./configs/cifar10_bpda_eot_sigma025_eot15.yml --eot_test 200 --attack apgd --loss cw --n_iter 10 --sigma_def .25 --max_iters_def 10 --model Standard --indices 0-45 $ebm_dir
 
 # test points with the full defense
-CUDA_VISIBLE_DEVICES=0 python3 eval_tta.py --model Standard --n_ex 1000  --dataset cifar10 $data_path --model_dir ../../robustbench-addmodels/robustbench/models --config ./configs/cifar10_bpda_eot_sigma025_eot15.yml --eot_test 5 --batch_size 1000 --attack test_points  --sigma_def .25 --max_iter 10 $imgs_path
-
+CUDA_VISIBLE_DEVICES=0 python3 eval_tta.py --model Standard --n_ex 1000  --dataset cifar10 $data_path $model_dir --config ./configs/cifar10_bpda_eot_sigma025_eot15.yml --eot_test 5 --batch_size 1000 --attack test_points  --sigma_def .25 --max_iters_def 10 $imgs_path $ebm_dir
 
 
 wait
